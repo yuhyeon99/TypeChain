7c3eb4473f1ec42df2fbc97adb1d7ff266d956f7
yuhyeon99 <jujini31@gmail.com>
2023-08-23 16:13:58 +0900

    feat : 블록체인 코드 완성

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,58 @@
-import {init, exit} from "myPackage";
+import crypto from "crypto";
 
-init({
-    url:"test"
-});
+interface BlockShape{
+    hash : string;
+    prevHash: string;
+    height : number;
+    data : string;
+}
 
-exit(1)
+class Block implements BlockShape{
+    public hash:string;
+    constructor(
+        public prevHash : string,
+        public height : number,
+        public data : string
+    ){
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
 
-localStorage
+    static calculateHash(prevHash:string, height:number, data:string){
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
+    }
 
-// class Block {
-//     constructor(private data:string ){}
-//     static hello(){
-//         return "hi";
-//     }
-// }
+}
+
+class Blockchain{
+    private blocks:Block[]
+    constructor(){
+        this.blocks = [];
+    }
+
+    // 이전 hash 값 가져오는 함수
+    private getPrevHash(){
+        if(this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length-1].hash;
+    }
+
+    // 새 블록 생성
+    public addBlock(data:string){
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+
+    public getBlocks(){
+
+        return [...this.blocks];
+    }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+console.log(blockchain.getBlocks());
